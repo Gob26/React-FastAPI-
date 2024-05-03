a9144e70c499636146adb62cacffa8e043f1a633
gob26 <profservis26@yandex.ru>
2024-05-03 22:03:42 +0300

    vite + react все очистил, белый лист

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import fastapi
-from src.router import router as router_crypto
-from src.config import settings
-from src.http_client import CMCClient
-
-app = fastapi.FastAPI()
-
-app.include_router(router_crypto) #добавляем роутер криптовалют
-
