5491887f4aa9770c3b3690e4ed9ccaf7d15b4593
gob26 <profservis26@yandex.ru>
2024-05-03 12:42:28 +0300

    добавляем в main наш роутер по крипте

--- src/main.py
+++ src/main.py
@@ -1,20 +1,9 @@
 import fastapi
-
+from src.router import router as router_crypto
 from src.config import settings
 from src.http_client import CMCClient
 
 app = fastapi.FastAPI()
 
-cmc_client = CMCClient(
-    base_url="https://pro-api.coinmarketcap.com",
-    api_key=settings.CMC_API_KEY
-)
+app.include_router(router_crypto) #добавляем роутер криптовалют
 
-@app.get("/cryptocurrencies")
-async def get_cryptocurrencies():
-    return await cmc_client.get_listings()
-
-
-@app.get("/cryptocurrency/{currency_id}")
-async def get_currency(currency_id: int):
-    return await cmc_client.get_currency(currency_id)
